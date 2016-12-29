4f0751d4bbeab18e60a89a4f401a563bfc4add8e
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-12-29 11:03:11 +0100

    Add comments

--- lib/swagger/docs/generator/configuration.rb
+++ lib/swagger/docs/generator/configuration.rb
@@ -4,9 +4,24 @@
 module Swagger
   module Docs
     module Generator
-      # # Configuration to gem
+      # # Configuration to swagger json file
       #
       # This class it's a DSL class. For customize default json file.
+      #
+      # @example
+      #   Swagger::Docs::Generator.configure |config|
+      #     config.version_swagger = '2.0.0'
+      #     config.version_api = '1.0.0'
+      #     config.base_path = 'api.example.com'
+      #     config.attribute = {
+      #       info: {
+      #         title: 'Topisime API',
+      #         description: 'Super Topisime API for http://example.com service'
+      #       }
+      #     }
+      #   end
+      #
+      # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md
       class Configuration
         attr_accessor :version_swagger, :version_api, :base_path, :attribute
 

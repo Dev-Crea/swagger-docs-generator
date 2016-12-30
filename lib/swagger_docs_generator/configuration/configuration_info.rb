28adef67738544facceca807d90ab5c6b244bc49
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-12-30 15:33:59 +0100

    Add comments

--- lib/swagger_docs_generator/configuration/configuration_info.rb
+++ lib/swagger_docs_generator/configuration/configuration_info.rb
@@ -2,6 +2,11 @@
 
 # :reek:Attribute
 module SwaggerDocsGenerator
+  # # Information element
+  #
+  # Create a Hash element for information to API doc.
+  #
+  # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#info-object
   class ConfigurationInfo
     attr_accessor :title, :description, :termsOfService, :contact, :license,
                   :version
@@ -9,6 +14,7 @@ module SwaggerDocsGenerator
     TITLE = 'Title Example API'
     VERSION = '1.0.0'
 
+    # Initialize required element
     def initialize
       @title = TITLE
       @version = VERSION

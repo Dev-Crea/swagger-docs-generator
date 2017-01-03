c7196d39a73aa495641ae2b0ae51973726c22a1d
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2017-01-03 17:41:37 +0100

    Fix generator actions

--- lib/swagger_docs_generator/methods.rb
+++ lib/swagger_docs_generator/methods.rb
@@ -1,18 +1,41 @@
 # frozen_string_literal: true
 
+# :reek:TooManyStatements and :reek:UtilityFunction
 module SwaggerDocsGenerator
   module Methods
-    def swagger_controller(_description)
-      # Create json file for controller
-      path = File.join(Dir.pwd, '/public')
-      version_api = SwaggerDocsGenerator.configure_info.version
-      json_file = "#{self.controller_name}.json"
-      FileUtils.touch(File.join(path, version_api, json_file))
+    # Create json file for controller
+    def swagger_controller(controller, _description)
+      FileUtils.touch(controller_file(controller))
     end
 
-    def swagger_doc(action, &block)
-      puts "Action : #{action}"
-      puts "Block : #{block}"
+    # Complete json file with datas to method and controller, controller reading
+    def swagger_doc(ctrl, action, data = {})
+      ctr_file = controller_file(ctrl)
+      json = File.read(ctr_file)
+      hash = json.blank? ? {} : JSON.parse(json)
+      File.open(ctr_file(ctrl), 'w') do |file|
+        file.puts(hash.merge!(construct_routes(ctrl, action, data)).to_json)
+      end
+    end
+
+    private
+
+    def controller_file(ctrl)
+      path = File.join(Dir.pwd, '/public')
+      version_api = SwaggerDocsGenerator.configure_info.version
+      File.join(path, version_api, "#{ctrl.controller_name}.json")
+    end
+
+    def construct_routes(controller, action, data)
+      verb = SwaggerDocsGenerator::Extractor.new(controller, action).verb
+      {
+        "/#{action}": {
+          verb => {
+            summary: data[:summary],
+            description: data[:description]
+          }
+        }
+      }
     end
   end
 end

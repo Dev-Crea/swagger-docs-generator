211d3a49bae694ba2ecd21a29faf07ebb68e342c
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2017-01-06 16:17:59 +0100

    Update response

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [0.2.0] - 2017-01-06
 ### [Changed]
+- Update responses
 - Update parameters
 
 ### [Added]
--- lib/swagger_docs_generator/parser/actions/response.rb
+++ lib/swagger_docs_generator/parser/actions/response.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# :reek:UtilityFunction
+
 module SwaggerDocsGenerator
   module Actions
     # # Test :response
@@ -16,9 +18,24 @@ module SwaggerDocsGenerator
       private
 
       def complete_hash(data)
-        default = { ok: { description: 'Request successfully processed.' } }
-        raw = data.key?(key) ? data[key] : default
-        hash[key] = raw
+        raw = data[key]
+        hash[key] = raw.present? ? each_response(raw) : no_response
+      end
+
+      def each_response(raw)
+        sh = {}
+        raw.each do |code|
+          sh.merge!(one_response(ActionDispatch::Response.new(code)))
+        end
+        sh
+      end
+
+      def no_response
+        one_response(ActionDispatch::Response.new(200))
+      end
+
+      def one_response(response)
+        { response.code.to_s => { description: response.message } }
       end
     end
   end

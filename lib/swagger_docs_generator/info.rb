d860198f4ca09dba46c1e001af17fb6ef4dfb3cc
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-03-23 14:45:36 +0100

    Fix checksum [ci skip]

--- lib/swagger_docs_generator/info.rb
+++ lib/swagger_docs_generator/info.rb
@@ -5,7 +5,7 @@ module SwaggerDocsGenerator
   # Name to gem
   GEM =           'swagger_docs_generator'
   # Version to gem
-  VERSION =       '0.3.0'
+  VERSION =       '0.3.1'
   # Authors contributed to gem
   AUTHORS =       ['VAILLANT Jeremy'].freeze
   # Email to ahuthors

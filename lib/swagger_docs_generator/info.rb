5b950b98fab3c313facc3f68b4623585b55e4ed5
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-05-12 12:41:53 +0200

    Increment version

--- lib/swagger_docs_generator/info.rb
+++ lib/swagger_docs_generator/info.rb
@@ -5,7 +5,7 @@ module SwaggerDocsGenerator
   # Name to gem
   GEM =           'swagger_docs_generator'
   # Version to gem
-  VERSION =       '0.4.0'
+  VERSION =       '0.5.0'
   # Authors contributed to gem
   AUTHORS =       ['VAILLANT Jeremy'].freeze
   # Email to ahuthors
